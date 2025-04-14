518ab36a3ae3a7ea0927604e27116348aab44632
Anand Otiv <w0858857@myscc.ca>
2025-04-14 13:59:28 -0400

    Initial Commit

---
+++ FinalTakeHomeProject/FinalTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalTakeHomeProjectApp.swift
+//  FinalTakeHomeProject
+//
+//  Created by Anand Otiv on 2025-04-14.
+//
+
+import SwiftUI
+
+@main
+struct FinalTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
