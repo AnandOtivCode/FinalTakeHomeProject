172618a729055bfdd5d3d0d7e048fcff2141a68d
Anand Otiv <w0858857@myscc.ca>
2025-04-16 19:30:06 -0400

    Started ProjectView, added images and made wheelpicker

---
+++ ProjectView.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectView.swift
+//  FinalTakeHomeProject
+//
+//  Created by Anand Otiv on 2025-04-16.
+//
+
+import SwiftUI
+
+struct ProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectView()
+}
