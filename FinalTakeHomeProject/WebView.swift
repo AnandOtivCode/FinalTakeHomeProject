2f3e6b003ae554cdbb7acbd07af7a991dc8f39b0
Anand Otiv <w0858857@myscc.ca>
2025-04-16 23:26:45 -0400

    Added NavigationLink to StClair and WebView

---
+++ FinalTakeHomeProject/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  FinalTakeHomeProject
+//
+//  Created by Anand Otiv on 2025-04-16.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
