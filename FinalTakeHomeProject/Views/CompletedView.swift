4abf2abc7874cc613f50f44055e05d2607fbc87d
Anand Otiv <w0858857@myscc.ca>
2025-04-17 20:33:22 -0400

    Got the basic layout of the CompletedView done and added it as a tab to entry view

---
+++ FinalTakeHomeProject/Views/CompletedView.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedView.swift
+//  FinalTakeHomeProject
+//
+//  Created by Anand Otiv on 2025-04-17.
+//
+
+import SwiftUI
+
+struct CompletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedView()
+}
